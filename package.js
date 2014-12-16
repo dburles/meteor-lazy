ffefbf66b653cd0c661963d16916e1671cdcb155
David Burles <dburles@gmail.com>
2014-12-16 11:15:22 +1100

    updated for Meteor 1.0

--- package.js
+++ package.js
@@ -1,10 +1,12 @@
 Package.describe({
-  summary: 'A handy little development tool'
+  name: 'dburles:lazy',
+  summary: 'A handy little development tool',
+  version: '1.0.0',
+  git: 'https://github.com/dburles/meteor-lazy.git'
 });
 
-Package.on_use(function(api) {
-  api.add_files('lazy.js', ['client', 'server']);
-  if (typeof api.export !== 'undefined') {
-    api.export('l');
-  }
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.addFiles('lazy.js');
+  api.export('l');
 });
