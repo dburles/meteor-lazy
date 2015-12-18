5cc4349db77c6f3ea1ba5bb8d443daf9c05b1c9c
David Burles <dburles@gmail.com>
2015-12-18 22:54:48 +1100

    declare underscore dependency

--- package.js
+++ package.js
@@ -1,12 +1,13 @@
 Package.describe({
   name: 'dburles:lazy',
   summary: 'A handy little development tool',
-  version: '1.0.0',
+  version: '1.0.1',
   git: 'https://github.com/dburles/meteor-lazy.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
+  api.use('underscore');
   api.addFiles('lazy.js');
   api.export('l');
 });
